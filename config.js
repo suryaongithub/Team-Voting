4b9352b6cb24632adb9c471f25317c6afc998298
suryaongithub <suryaakshajk@outlook.com>
2023-01-12 15:33:54 +0530

    stuff-added

--- config.js
+++ config.js
@@ -3,6 +3,13 @@ import firebase from 'firebase';
 // add SDK Firebase
 var firebaseConfig = {
 
+    apiKey: "AIzaSyCemZkG7zRzo54P31MBKP2d1YvBGQGWh6M",
+    authDomain: "team-voting-cbbee.firebaseapp.com",
+    projectId: "team-voting-cbbee",
+    storageBucket: "team-voting-cbbee.appspot.com",
+    messagingSenderId: "6733689714",
+    appId: "1:6733689714:web:8656da253554cdf81d63eb",
+    measurementId: "G-JQ9257H8BW"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
